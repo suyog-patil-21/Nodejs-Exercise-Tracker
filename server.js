de0c676e7848d072211e66fed8e2e9db8e5131c9
MyUbuntu <suyog0057@gmail.com>
2021-10-01 23:58:48 +0530

    stable 0.0.4

--- server.js
+++ server.js
@@ -12,8 +12,11 @@ mongoose
     useNewUrlParser: true,
     useUnifiedTopology: true,
   })
+  .then(() => {
+    console.log("Connected to Database");
+  })
   .catch((er) => {
-    console.log("Mongoose Connection error => ", er);
+    console.log("Mongoose Connection failed Error => ", er);
   });
 
 app.use(express.static("public"));
@@ -202,16 +205,3 @@ app.get("/api/users/:_id/logs?", async (req, res) => {
 const listener = app.listen(process.env.PORT || 3000, () => {
   console.log("Your app is listening on port " + listener.address().port);
 });
-//fre : 61543c0982372104c8fa26dc
-//me : 61543c0eebd9dd40e0a36f15
-
-/**
- * You can make a GET request to /api/users/:_id/logs to retrieve a full exercise log of any user.
-A request to a user's log GET /api/users/:_id/logs returns a user object with a count property representing the number of exercises that belong to that user.
-A GET request to /api/users/:id/logs will return the user object with a log array of all the exercises added.
-Each item in the log array that is returned from GET /api/users/:id/logs is an object that should have a description, duration, and date properties.
-The description property of any object in the log array that is returned from GET /api/users/:id/logs should be a string.
-The duration property of any object in the log array that is returned from GET /api/users/:id/logs should be a number.
-The date property of any object in the log array that is returned from GET /api/users/:id/logs should be a string.. Use the dateString format of the Date API.
-You can add from, to and limit parameters to a GET /api/users/:_id/logs request to retrieve part of the log of any user. from and to are dates in yyyy-mm-dd format. limit is an integer of how many logs to send back.
- */
